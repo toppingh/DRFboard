db812454814fcf535a3ee02e54f998214cf6d0a5
ekdms <reyna11@naver.com>
2023-07-19 05:19:39 +0900

    0719_로그인/로그아웃 기능 구현

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+#from rest_framework.authtoken.admin import User
+from django.contrib.auth.models import User
+
+
+from .serializers import RegisterSerializer, LoginSerializer
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data
+        return Response({'token': token.key}, status=status.HTTP_200_OK)
